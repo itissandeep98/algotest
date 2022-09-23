982695062013c6cc63b9d019b7912da475aae485
Sandeep Kumar Singh <itissandeep98@gmail.com>
2022-09-23 11:50:04 +0530

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import LayoutWrapper from '../layouts/LayoutWrapper';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <LayoutWrapper {...pageProps}>
+      <Component {...pageProps} />
+    </LayoutWrapper>
+  );
+}
+
+export default MyApp;
